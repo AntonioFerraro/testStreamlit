6e3e3a5d99038924f0c6f0446d818ee8ad44ab73
ANTONIO FERRARO <antonio.ferraro@adeccogroup.com>
2023-05-05 16:22:59 +0200

    Big Massi

--- app/main.py
+++ app/main.py
@@ -2,7 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Test Dashboard')
-
-st.components.v1.html(r"""<iframe title="Job Prioritization - Streamlit" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=bdb98b9a-9f75-4679-afa2-a5d216854a10&autoAuth=true&ctid=f30ac191-b8b4-45f2-9a9b-e5466cb90c2f" frameborder="0" allowFullScreen="true"></iframe>""", width=1280, height=720, scrolling=True)
+st.title("Hello World!")
 
